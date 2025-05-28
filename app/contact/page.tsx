0336b94911d3c648c05ef1e605ece474794bb712
harris alam <harrisalam3@gmail.com>
2025-05-28 12:11:07 +0100

    Update contact page social links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,26 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import {Github, Linkedin, Mail, Twitter} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/harris-alam-7a0550113/",
+		label: "LinkedIn",
+		handle: "Visit my LinkedIn Profile",
+	},{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:harrisalam3@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "harrisalam3@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Harris-A",
 		label: "Github",
-		handle: "chronark",
+		handle: "Visit my Github Profile",
 	},
 ];
 
